e278e757cbd14b67c5c35fadd83b28fd0ff7c10a
Mariana <gtz.mariana26@gmail.com>
2024-04-17 14:42:43 -0700

    Agregar el componente SearchBar aApp.jsx

---
+++ pictures/src/api.js
@@ -0,0 +1,14 @@
+import axios from  'axios'
+
+const searchImage = async(term)=>{
+    const url = 'https://api.unsplash.com/search/photos/?client_id=6tOYXzh2nzaWSS2_6BL0Jmx-rBQDTWN2KrP2UugZYew'
+    const response = await axios.get(url, {
+        params: {
+            query :  ' cars'
+
+        }
+    })
+    console.log(response)
+}
+
+export default searchImage
