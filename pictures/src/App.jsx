884d26bcf66932c2dc826fafc92453afe6872ebe
marianagtzs <gtz.mariana26@gmail.com>
2024-04-24 13:56:26 -0700

    Modifiqué el archivo Api.js

--- pictures/src/App.jsx
+++ pictures/src/App.jsx
@@ -1,18 +1,20 @@
 import { useState } from 'react'
 import SearchBar from './components/SearchBar'
-import ImageList from 'components/ImageList'
-import SearchImage from './api'
+import ImageList from './components/ImageList'
+import searchImage from './api'
 import './App.css'
 
 function App() {
-  const [Image, setImages] = useState([])
+  const [image, setImages] = useState([])
 
   const handleSubmit = async(term) => {
-    console.log('Usted está buscando con :' , term)
-  }
+  console.log('Usted está buscando con :' , term)
+  const result = await searchImage(term)
+  setImages(result)}
   return (
     <div><h1>App</h1>
     <SearchBar onSubmit={handleSubmit}/>
+    <ImageList images={image} />
     </div>
   )
 } 
--- pictures/src/api.js
+++ pictures/src/api.js
@@ -1,7 +1,7 @@
 import axios from  'axios'
 
 const searchImage = async(term)=>{
-    const url = 'https://api.unsplash.com/search/photos/?client_id=pk_BUEZM07iI6XLWeOV-8B3CMbYevrgqgtuu5_t8GYk'
+    const url = 'https://api.unsplash.com/search/photos/?client_id=365cH_K1AghQclsvRIy0n6YZIQgwiuaK-EePOUeO3_A'
     const response = await axios.get(url, {
         params: {
             query :  term
